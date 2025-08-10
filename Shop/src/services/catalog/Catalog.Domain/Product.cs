212fb2ff92103ee839231b687294b729e3e32073
orkun.kahraman <orkun.kahraman@ardgrup.com.tr>
2025-08-10 17:07:11 +0300

    added product

---
+++ Shop/src/services/catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.Domain;
+
+public sealed class Product
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+
+    private Product() { }
+    public Product(string name, decimal price, int stock)
+    {
+        Name = name;
+        Price = price;
+        Stock = stock;
+    }
+
+    public void DecreaseStock(int qty)
+    {
+        if (qty <= 0 || qty > Stock)
+        {
+            throw new InvalidOperationException("Invalid qty");
+        }
+
+        Stock -= qty;
+    }
+}
