1b406f0848cd213cc29cb0499c2b4e04500bae65
orkun.kahraman <orkun.kahraman@ardgrup.com.tr>
2025-08-10 17:49:18 +0300

    added di

---
+++ Shop/src/services/catalog/Catalog.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Application.Products.Contracts;
+using Catalog.Infrastructure.Persistence;
+using Catalog.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infrastructure.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCatalogInfrastructure(this IServiceCollection services, string? connectionString)
+    {
+        services.AddDbContext<CatalogDbContext>(opt => opt.UseSqlServer(connectionString));
+        services.AddScoped<IProductRepository, EfProductRepository>();
+        return services;
+    }
+}
