122dc18579da5054c8deb7f0aceefa739e146244
orkun.kahraman <orkun.kahraman@ardgrup.com.tr>
2025-08-10 17:52:43 +0300

    added connection

---
+++ Shop/src/services/catalog/Catalog.Infrastructure/Persistence/CatalogDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Catalog.Infrastructure.Persistence;
+
+public sealed class CatalogDbContextFactory : IDesignTimeDbContextFactory<CatalogDbContext>
+{
+    public CatalogDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<CatalogDbContext>()
+            .UseSqlServer("connnn")
+            .Options;
+
+        return new CatalogDbContext(options);
+    }
+}
